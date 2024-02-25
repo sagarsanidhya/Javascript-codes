87d12fd8531e389b64e02c7cea609193140f7c92
sagarsanidhya <sagarsanidhya@gmail.com>
2024-02-25 20:56:18 +0530

    Learned more about the datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,32 @@
+"use strict";  
+// treat all JS codes as newer version
+
+//alert("hello")
+//we are using nodeJS not browser
+
+//console.log(3+3); console.log("hello")
+//Not a correct way to write as we have to maintain readability
+//not a good practice
+
+console.log(3+3)
+console.log("hello")
+
+let name = "SP"
+let age ="21"
+let isLoggedIn = false
+
+// number =>2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null=> standalone value
+// undefined =>
+// symbol => unique 
+
+// object
+
+console.log(typeof age);
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
