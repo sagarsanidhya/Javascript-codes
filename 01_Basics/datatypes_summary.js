c7db9995b49e4d9e5ddb953116899ecd48f28819
sagarsanidhya <sagarsanidhya@gmail.com>
2024-02-26 19:10:42 +0530

    dataTypes summary

--- 01_Basics/datatypes_summary.js
+++ 01_Basics/datatypes_summary.js
@@ -1,5 +1,27 @@
-//Divided on the basis of how they are stored in the memory and 
+//Divided on the basis of how they are stored in the memory and how they can be accessed
 
-// Primitive  
-// These are call by value
-// 7 categories : string ,Number , Boolean , Null , Undefined , symbol
+// Primitive  / Value type
+// These are call by value / Not copied by memory
+// 7 categories : string , Number , Boolean , Null , Undefined , symbol ,BigInt
+
+// Non -Primitive / Refrence type
+// arrays , Objects, functions
+
+// JS is statically typed or dynamic type?
+
+//Declaration of Non-Primitive Datatypes
+
+const heros = ["shaktimaan","naagraj","doga"] //Array
+
+let myObj ={
+    name: "Rauy",
+    age: 22,
+} //Object
+
+const myFunction = function(){
+    console.log("Hello World");
+} //Function
+
+console.log(typeof myFunction);
+console.log(typeof heros);
+console.log(typeof myObj);
