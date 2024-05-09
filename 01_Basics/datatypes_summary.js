f866878e7a4ebfd078dc061520ff31f94b4364bb
sagarsanidhya <sagarsanidhya@gmail.com>
2024-05-09 23:42:12 +0530

    stack and Heap Memory

--- 01_Basics/datatypes_summary.js
+++ 01_Basics/datatypes_summary.js
@@ -26,3 +26,35 @@ console.log(typeof myFunction);
 console.log(typeof heros);
 console.log(typeof myObj);
 
+// ----------------------------------------
+
+// Two types of memory 
+// stack and Heap Memory
+
+// stack(primitive), heap memory(Non-primitive)
+
+let  myYoutubename = "Rauy"
+
+let anothername = myYoutubename
+anothername ="chaiaurcode"
+
+console.log(myYoutubename);
+
+console.log(anothername);
+
+//let leke anothername jab declare hua to uska copy aagya myyoutube name ka, naki real data hence
+// karke check kar sakte hai.  hence primitive data type.
+
+//But in case of heap memory copy nahi refrence jayega for example
+
+let userone ={
+    email: "userone@gmail.com",
+    upi: "upi@ybl",
+}
+
+let usertwo = userone
+
+usertwo.email = "usertwo@gmail.com"
+
+console.log(userone.email);
+console.log(usertwo.email);
