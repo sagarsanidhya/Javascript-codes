3a3ad77bfd6bda6c360740f56404a073f51c9f39
sagarsanidhya <sagarsanidhya@gmail.com>
2024-05-21 00:01:27 +0530

    Object 2

--- 02_Basics/04_Objects.js
+++ 02_Basics/04_Objects.js
@@ -26,5 +26,12 @@ const obj1 = {1: "a", 2: "b", 3: "c", 4: "d"}
 const obj2 = {5: "e", 6: "f", 7: "g", 8: "h"}
 
 //const obj3 = {obj1, obj2}
-const obj3 = Object.assign(obj1,obj2) //Copy the values of all of the enumerable own properties from one or more source objects to a target object. Returns the target object.
+//const obj3 = Object.assign({},obj1,obj2) //Copy the values of all of the enumerable own properties from one or more source objects to a target object. Returns the target object.
+const obj3 = {...obj1, ...obj2} //spread operator is mainly used
 console.log(obj3);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));  // to check wether the element is present on not
